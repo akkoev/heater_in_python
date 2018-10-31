a4436a2d140882e64c302ae3c10905f50dd7c21b
Anton Verbeek <anton.verbeek@cooll.eu>
2018-10-31 16:17:49 +0100

    working setup.py

--- heatermod/setup.py
+++ heatermod/setup.py
@@ -6,7 +6,7 @@ from distutils import sysconfig
 cpp_args = ['-std=c++11', '-stdlib=libc++', '-mmacosx-version-min=10.7']
 
 sfc_module = Extension(
-    'heatermod', sources = ['modules.cpp'],
+    'heatermod', sources = ['module.cpp'],
     include_dirs=['pybind11/include'],
     language='c++',
     extra_compile_args = cpp_args,
